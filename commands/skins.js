11fcd73a39adbcb639be1f67561ab87d8c9c5afc
Vladimír Pokorný <justleader@justleader.eu>
2022-05-29 09:54:40 +0200

    Fixed typo and added a server
    Fixed typo in skins.js
    Added a server to servers.js

--- commands/skins.js
+++ commands/skins.js
@@ -12,7 +12,7 @@ module.exports = {
 		const exampleEmbed = new MessageEmbed()
 		.setColor('#5ACFF5')
 		.setTitle('Skins')
-		.setDescription('**Customize your NPC settings menu skin!**\n➱ Go to MineSkin\n➱ Choose the skin you want\n➱ Click on it, and copy the code\nthat is in "Direct Link" after https://minesk.in/\n➱ Insert in into config.yml under the MineSkin function\n➱ Restart the server, and you are done!')
+		.setDescription('**Customize your NPC settings menu skin!**\n➱ Go to MineSkin\n➱ Choose the skin you want\n➱ Click on it, and copy the code\nthat is in "Direct Link" after https://minesk.in/\n➱ Insert it into config.yml under the MineSkin function\n➱ Restart the server, and you are done!')
 		.setAuthor({ name: 'KnockioFFA', iconURL: 'https://i.imgur.com/jiSUxRz.png', url: 'https://www.mc-market.org/resources/20068/' })
 		.setTimestamp()
 		.setFooter({ text: 'KnockiBot made by JustLeader#5532', iconURL: 'https://i.imgur.com/amZUuhG.png' });
