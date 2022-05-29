24557ba714fc4c28976795dd8e621610d894c46b
Vladimír Pokorný <justleader@justleader.eu>
2022-05-29 08:59:21 +0200

    Didn't get saved last time

--- commands/placeholders.js
+++ commands/placeholders.js
@@ -12,7 +12,7 @@ module.exports = {
 		const exampleEmbed = new MessageEmbed()
 		.setColor('#6DE194')
 		.setTitle('Placeholders')
-		.setDescription('No download needed\n➱ %knc_kills%\n➱ %knc_deaths%\n➱ %knc_streak%\n➱ %knc_maxstreak%\n➱ %knc_kdr%\n➱ %knc_elo%\n➱ %knc_elorank%\n➱ %knc_elotag%\n➱ %knc_killeffect%\n➱ %knc_killsound%\n➱ %knc_trail%\n➱ %knc_block%\n➱ %knc_map%\n➱ %knc_maptimer%')
+		.setDescription('**No download needed**\n➱ %knc_kills%\n➱ %knc_deaths%\n➱ %knc_streak%\n➱ %knc_maxstreak%\n➱ %knc_kdr%\n➱ %knc_elo%\n➱ %knc_elorank%\n➱ %knc_elotag%\n➱ %knc_killeffect%\n➱ %knc_killsound%\n➱ %knc_trail%\n➱ %knc_block%\n➱ %knc_map%\n➱ %knc_maptimer%')
 		.setAuthor({ name: 'KnockioFFA', iconURL: 'https://i.imgur.com/jiSUxRz.png', url: 'https://www.mc-market.org/resources/20068/' })
 		.setTimestamp()
 		.setFooter({ text: 'KnockiBot made by JustLeader#5532', iconURL: 'https://i.imgur.com/amZUuhG.png' });
