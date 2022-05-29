03b706f3d53b138c2e5cfffd855711d5b4dfc5df
Vladimír Pokorný <justleader@justleader.eu>
2022-05-29 20:29:15 +0200

    Added time in two more places

--- events/suggestions.js
+++ events/suggestions.js
@@ -4,12 +4,14 @@ module.exports = {
 	name: 'messageCreate',
 	execute(message) {
 		if(message.channel == suggestion_channel_id){
-            console.log("Adding suggestions channel reactions onto message with ID " + message.id + "in the configured suggestions channel.")
+            let timestamp = Date.now();
+            console.log(Math.floor(timestamp/1000) + " | Adding suggestions channel reactions onto message with ID " + message.id + "in the configured suggestions channel.")
             try {
             message.react('accept:845429993701244989');
             message.react('deny:842180487903707176');
             }catch{
-                console.log("Failed at adding suggestions to message with ID " + message.id + " in the configured suggestions channel.")
+                let timestamp = Date.now();
+                console.log(Math.floor(timestamp/1000) + " | Failed at adding suggestions to message with ID " + message.id + " in the configured suggestions channel.")
             }
         }
 	},
