24f7efd50d8fe1f921f93a542238da58d0788109
Vladimír Pokorný <justleader@justleader.eu>
2022-06-04 22:27:39 +0200

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -12,7 +12,7 @@ module.exports = {
 		const exampleEmbed = new MessageEmbed()
 		.setColor('#5ACFF5')
 		.setTitle('Servers')
-		.setDescription('**The official KnockioFFA MC server:**\n➱ knockioffa.justleader.eu\n**Other servers using this plugin:**\n➱ mc.voxcraft.us\n➱ play.nzcraft.net\n➱ mc.novasurvival.it\n➱ play.vencagames.cz\n➱ mc.uranocraft.net\n➱ mc.mushroomnet.it')
+		.setDescription('**The official KnockioFFA MC server:**\n➱ knockioffa.justleader.eu\n\n**Other servers using this plugin:**\n➱ mc.voxcraft.us\n➱ play.nzcraft.net\n➱ mc.novasurvival.it\n➱ play.vencagames.cz\n➱ mc.uranocraft.net\n➱ mc.mushroomnet.it')
 		.setAuthor({ name: 'KnockioFFA', iconURL: 'https://i.imgur.com/jiSUxRz.png', url: 'https://www.mc-market.org/resources/20068/' })
 		.setFooter({ text: 'KnockiBot made by JustLeader#5532', iconURL: 'https://i.imgur.com/amZUuhG.png' });
 
